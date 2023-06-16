775490e6c7c9befa35203a366bfa6875fad3b2e3
Said Mazaya <117622294+saidmazaya@users.noreply.github.com>
2023-06-16 21:46:08 +0700

    Model Sementara

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Vote extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'user_id',
+        'discussion_id',
+    ];
+
+    public function discussion()
+    {
+        return $this->belongsTo(Discussion::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
