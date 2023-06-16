5b548d5730a297f39b8d4e05eb03f38c31f034f7
RafiDevari <rafidevari2004@gmail.com>
2023-06-16 10:26:10 +0700

    Menambah landing page dan about

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -6,31 +6,12 @@
 
 <nav id="navbar" class="navbar">
   <ul>
-    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-    <li><a class="nav-link scrollto" href="#about">About</a></li>
-    <li><a class="nav-link scrollto" href="#services">Services</a></li>
-    <li><a class="nav-link scrollto o" href="#portfolio">Portfolio</a></li>
-    <li><a class="nav-link scrollto" href="#team">Team</a></li>
-    <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-    <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-      <ul>
-        <li><a href="#">Drop Down 1</a></li>
-        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-          <ul>
-            <li><a href="#">Deep Drop Down 1</a></li>
-            <li><a href="#">Deep Drop Down 2</a></li>
-            <li><a href="#">Deep Drop Down 3</a></li>
-            <li><a href="#">Deep Drop Down 4</a></li>
-            <li><a href="#">Deep Drop Down 5</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Drop Down 2</a></li>
-        <li><a href="#">Drop Down 3</a></li>
-        <li><a href="#">Drop Down 4</a></li>
-      </ul>
-    </li>
-    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-    <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+    <li><a class="nav-link scrollto" href="#hero">Home</a></li>
+    <li><a class="nav-link scrollto" href="#about">Forum Diskusi</a></li>
+    <li><a class="nav-link scrollto" href="#services">Informasi</a></li>
+    <li><a class="nav-link scrollto o" href="#footer">About</a></li>
+   
+    <li><a class="getstarted scrollto" href="#hero">Login</a></li>
   </ul>
   <i class="bi bi-list mobile-nav-toggle"></i>
 </nav>
