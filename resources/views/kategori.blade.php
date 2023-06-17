43586ae3bea1901b903c2925b30eac4ddfeed5e8
Said Mazaya <117622294+saidmazaya@users.noreply.github.com>
2023-06-17 12:25:58 +0700

    Kategori (Search, FIlter by Letter)

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -1,56 +1,76 @@
 @extends('layout.home')
 
+@section('title', 'Kategori')
+
 @section('konten')
-    <!-- ======= Breadcrumbs ======= -->
-    <main id="main">
-    <section class="breadcrumbs">
-        <div class="container">
-          
-  
-        </div>
-      </section><!-- End Breadcrumbs -->
-  
-      <section class="inner-page">
-        <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
-            <div class="d-flex justify-content-between align-items-center section-title">
-                <h2>Kategori</h2>
-                {{-- <ol>
-                  <li><a href="index.html">Home</a></li>
-                  <li>Inner Page</li>
-                </ol> --}}
+<!-- ======= Breadcrumbs ======= -->
+<main id="main">
+  <section class="breadcrumbs">
+    <div class="container">
+
+
+    </div>
+  </section><!-- End Breadcrumbs -->
+
+  <section class="inner-page">
+    <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+      <div class="d-flex justify-content-between align-items-center section-title">
+        <h2>Kategori</h2>
+        {{-- <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Inner Page</li>
+        </ol> --}}
+      </div>
+      <p>
+        {{-- Category - START --}}
+
+        {{-- Search --}}
+        <div class="row mb-3">
+          <!-- Paginasi huruf A-Z di sebelah kiri -->
+          <div class="col-8 d-flex justify-content-start">
+            @foreach ($letters as $letter)
+              <a class="me-2" href="{{ route('kategori.index', ['letter' => $letter]) }}">{{ $letter }}</a>
+            @endforeach
+          </div>
+        
+          <!-- Form pencarian di sebelah kanan -->
+          <div class="col-4 d-flex justify-content-end">
+            <form action="{{ route('kategori.index') }}" method="GET" class="mb-3">
+              <div class="input-group">
+                <input type="text" name="search" class="form-control form-control-sm" placeholder="Cari kategori">
+                <button type="submit" class="btn btn-primary">Cari</button>
               </div>
-          <p>
-            {{-- ganti za - START --}}
-            <h3>Diabetes</h3>
-            <ul>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-            </ul>
-            <h3>Penyakit Jiwa</h3>
-            <ul>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-            </ul>
-            <h3>Jantung</h3>
-            <ul>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-                <li><a href=""><h5>tes<h5></a></li>
-            </ul>
-            {{-- ganti za - END --}}
-          </p>
+            </form>
+          </div>
         </div>
-      </section>
-      <section class="breadcrumbs">
-        <div class="container">
-          
-  
-        </div>
-      </section>
-    </main><!-- End #main -->
+        
+
+      <!-- Tampilkan kategori sesuai dengan halaman yang dipilih -->
+      <div class="mb-2">
+        @if ($paginator->isEmpty())
+            <p>Tidak ada kategori yang sesuai dengan pencarian.</p>
+        @else
+            @foreach ($paginator as $category)
+                <h6>
+                    <a class="fs-6" href="#">{{ $category->name }}</a>
+                </h6>
+            @endforeach
+        @endif
+    </div>
+    
+
+      <!-- Paginasi bawah dengan jumlah halaman -->
+      {{ $paginator->links() }}
+
+      {{-- Category - END --}}
+      </p>
+    </div>
+  </section>
+  <section class="breadcrumbs">
+    <div class="container">
+
+
+    </div>
+  </section>
+</main><!-- End #main -->
 @endsection
