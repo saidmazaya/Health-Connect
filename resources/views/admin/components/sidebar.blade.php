a81ecbb823f0096d5ba6de5b6b600b590cc42877
Said Mazaya <117622294+saidmazaya@users.noreply.github.com>
2023-06-17 15:08:48 +0700

    menyesuaikan

---
+++ resources/views/admin/components/sidebar.blade.php
@@ -0,0 +1,68 @@
+<!-- partial:partials/_sidebar.html -->
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/dashboard">
+                <i class="menu-icon fa-solid fa-table-cells-large"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item nav-category">Data Management</li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.discussion') }}" aria-expanded="false" aria-controls="ui-basic">
+                <i class="menu-icon fa-solid fa-heart-pulse"></i>
+                <span class="menu-title">Discussion</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.response') }}" aria-expanded="false" aria-controls="ui-basic">
+                <i class="menu-icon fa-solid fa-reply"></i>
+                <span class="menu-title">Response</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#comment-basic" aria-expanded="false" aria-controls="comment-basic">
+                <i class="menu-icon fa-solid fa-comment-slash"></i>
+                <span class="menu-title">Reports</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="comment-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="{{ route('report-discussion.index') }}">Report Discussion</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Report Response</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item nav-category">Forms and Datas</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" aria-expanded="false" aria-controls="form-elements">
+                <i class="menu-icon fa-solid fa-folder-plus"></i>
+                <span class="menu-title">Articles</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" aria-expanded="false" aria-controls="charts">
+                <i class="menu-icon fa-solid fa-tag"></i>
+                <span class="menu-title">Category</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" aria-expanded="false" aria-controls="tables">
+                <i class="menu-icon fa-solid fa-user"></i>
+                <span class="menu-title">User Table</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" aria-expanded="false" aria-controls="tables">
+                <i class="menu-icon fa-solid fa-user-doctor"></i>
+                <span class="menu-title">Doctor Table</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" aria-expanded="false" aria-controls="tables">
+                <i class="menu-icon fa-solid fa-stethoscope"></i>
+                <span class="menu-title">Specialist</span>
+            </a>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.layout.main')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+<!-- partial -->
+<div class="container-fluid page-body-wrapper">
+  <!-- partial:partials/_settings-panel.html -->
+  @include('admin.components.sidebar')
+
+  <!-- partial -->
+  <div class="main-panel">
+    <div class="content-wrapper">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="home-tab">
+            <div class="d-sm-flex align-items-center justify-content-between border-bottom">
+            </div>
+            <div class="tab-content tab-content-basic">
+              <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="statistics-details d-flex align-items-center justify-content-between">
+                        @php
+                        $articleCount = DB::table('articles')->count();
+                        $discussionCount = DB::table('discussions')->count();
+                        $userCount = DB::table('users')
+                        ->where('role_id', '!=', 1)
+                        ->count();
+                        $doctorCount = DB::table('users')
+                        ->where('role_id', 3)
+                        ->count();
+                        $categoryCount = DB::table('categories')->count();
+                        @endphp
+                      <div>
+                        <p class="statistics-title">Total User</p>
+                        <h3 class="rate-percentage">{{ $userCount }}</h3>
+                      </div>
+                      <div>
+                        <p class="statistics-title">Total Doctor</p>
+                        <h3 class="rate-percentage">{{ $doctorCount }}</h3>
+                      </div>
+                      <div>
+                        <p class="statistics-title">Total Article</p>
+                        <h3 class="rate-percentage">{{ $articleCount }}</h3>
+                      </div>
+                      <div>
+                        <p class="statistics-title">Total Discussion</p>
+                        <h3 class="rate-percentage">{{ $discussionCount }}</h3>
+                      </div>
+                      <div class="d-none d-md-block">
+                        <p class="statistics-title">Total Category</p>
+                        <h3 class="rate-percentage">{{ $categoryCount }}</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- content-wrapper ends -->
+    <!-- partial:partials/_footer.html -->
+    <footer class="footer">
+      <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Health Connect <a href="/" target="_blank">Discuss About Health</a> from Health Connect.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2023. All rights reserved.</span>
+      </div>
+    </footer>
+    <!-- partial -->
+  </div>
+  <!-- main-panel ends -->
+</div>
+<!-- page-body-wrapper ends -->
+@endsection
