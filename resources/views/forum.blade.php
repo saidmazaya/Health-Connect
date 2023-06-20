0c85111754d083364178ce8866828612451051ca
RafiDevari <rafidevari2004@gmail.com>
2023-06-20 09:03:08 +0700

    gabung lagi ama punya said

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -22,19 +22,21 @@
         </ol> --}}
       </div>
       <p>
+        <hr>
         {{-- Forum - START --}}
         @foreach ($parent as $data)
-      <h4 class="mb-3">{{ $data->name }}</h4>
-      <ul class="mb-5">
+      <h4 class="mb-3"><i>{{ $data->name }}</i></h4>
+      
         @foreach ($category->where('type_id', $data->id) as $item)
-        <li><a href="{{ route('kategori.detail', $item->slug) }}">
+        <a href="{{ route('kategori.detail', $item->slug) }}">
             <h5>{{ $item->name }}<h5>
-          </a></li>
+          </a>
         @endforeach
-      </ul>
+      <hr>
       @endforeach
       {{ $parent->links() }}
       {{-- Forum - END --}}
+
       </p>
     </div>
   </section>
